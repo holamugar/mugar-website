1f58fa370d5b7595475aec922ee4456bae2d18ce
Facundo Capua <facundocapua@Facundos-MacBook-Pro.local>
2022-10-06 19:24:38 -0300

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'MugAr';
+export const SITE_DESCRIPTION = 'Magento Users Group ARgentina';
